3b98041707a5884ce4921e851f09ead288fd573d
Stephane HERAULT <stephane.herault@gmail.com>
2026-04-03 21:30:14 +0200

    chore: add shared eslint-config package

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,17 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    },
+  }
+)
